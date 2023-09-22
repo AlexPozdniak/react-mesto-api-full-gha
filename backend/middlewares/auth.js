de70c49abb76f29921c26fdb13768042ac3b1118
AlexPozdniak <sotich911@gmail.com>
2023-09-22 19:03:49 +0300

    push 6

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,6 +6,8 @@ const Unauthorized = require('../errors/unauthorized');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  console.log(req.headers);
+  console.log(authorization);
 
   if (!authorization || !authorization.startsWith('Bearer')) {
     return next(new Unauthorized('Необходима авторизация!'));
